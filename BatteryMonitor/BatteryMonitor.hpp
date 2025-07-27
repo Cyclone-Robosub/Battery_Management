78f850ebcaefffa22f777d2767f859194555c503
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-07-27 08:25:20 -0700

    Formatting.

--- BatteryMonitor/BatteryMonitor.hpp
+++ BatteryMonitor/BatteryMonitor.hpp
@@ -1,11 +1,10 @@
 #ifndef BATTERYMONITOR_HPP
 #define BATTERYMONITOR_HPP
 
-
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/bool.hpp"
 #include <chrono>
 #include <iostream>
 using namespace std::chrono_literals;
@@ -22,12 +21,16 @@ using namespace std::chrono_literals;
 
 class BatteryMonitor : public rclcpp::Node {
 public:
-  BatteryMonitor() : Node("BatteryMonitorNode"), startupTime(std::chrono::steady_clock::now()) {
+  BatteryMonitor()
+      : Node("BatteryMonitorNode"),
+        startupTime(std::chrono::steady_clock::now()) {
     // SetupADCConnection();
     CurrentChargeSub = this->create_subscription<std_msgs::msg::Float64>(
-                "Current_Topic", 10, std::bind(&BatteryMonitor::getCurrent, this, std::placeholders::_1));
+        "Current_Topic", 10,
+        std::bind(&BatteryMonitor::getCurrent, this, std::placeholders::_1));
     CurrentVoltSub = this->create_subscription<std_msgs::msg::Float64>(
-                "Volt_Topic", 10, std::bind(&BatteryMonitor::getVolt, this, std::placeholders::_1));
+        "Volt_Topic", 10,
+        std::bind(&BatteryMonitor::getVolt, this, std::placeholders::_1));
     timeInital = startupTime;
     // StartupSOC using Voltage at a single time point
     previousSOC = (CurrentVolt - MINVOLT / (MAXVOLT - MINVOLT)) / 100;
@@ -37,14 +40,13 @@ public:
         this->create_publisher<std_msgs::msg::Float64>("SOCTopic", 10);
     SOCINTPublisher =
         this->create_publisher<std_msgs::msg::Bool>("SOCIntTopic", 10);
-    
 
     // This function also generates INT for the SOCPublisherINT. If the call to
     // this function is removed, then make sure to replace with some way of
     // checking for INT of SOC.
-    SOCtimer_ = this->create_wall_timer(std::chrono::milliseconds(100), [this]() {
-            this->SOC_RealTime_callback();
-        });
+    SOCtimer_ =
+        this->create_wall_timer(std::chrono::milliseconds(100),
+                                [this]() { this->SOC_RealTime_callback(); });
   }
   // Setup publisher to a SOC Topic.
 private:
@@ -55,16 +57,17 @@ private:
   std::chrono::steady_clock::time_point startupTime;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr SOCPublisher;
   rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr CurrentChargeSub;
-   rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr CurrentVoltSub;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr CurrentVoltSub;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr SOCINTPublisher;
   rclcpp::TimerBase::SharedPtr SOCtimer_;
+
   std::chrono::steady_clock::time_point timeInital;
   double currentCurrentValue;
   std::chrono::steady_clock::time_point timeFinal;
   const double currentRating = CURRENTRATING;
-  // Need a startup SOC.
   double previousSOC;
   double CalcSOC();
+
   void getVolt(const std_msgs::msg::Float64::SharedPtr msg);
   double CurrentVolt;
   void getCurrent(const std_msgs::msg::Float64::SharedPtr msg);

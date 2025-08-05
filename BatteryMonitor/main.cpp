1fcb2e8d4112083db50f1eed2f0326d18055e61e
flametd14 <107810692+StrongStaples786@users.noreply.github.com>
2025-08-05 06:37:50 -0700

    forgot to fix main.

--- BatteryMonitor/main.cpp
+++ BatteryMonitor/main.cpp
@@ -2,20 +2,21 @@
 #include <rclcpp/rclcpp.hpp>
 #include <thread>
 
-int main(int argc, char **argv) {
-  
+int main(int argc, char **argv)
+{
+
   rclcpp::init(argc, argv);
-  std::shared_ptr<BatteryMonitor>Batteryinstance = std::make_shared<BatteryMonitor>();
-  rclcpp::executors::MultiThreadedExecutor exec;
+  std::shared_ptr<BatteryMonitor> Batteryinstance = std::make_shared<BatteryMonitor>();
+  std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> exec = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
   exec->add_node(Batteryinstance);
-  std::jthread spin_ros([this]()
-{exec->spin();});
-  std::jthread Actual_Battery_Thread([this](){
-    Batteryinstance->Startup();
-  })
+  std::jthread spin_ros([exec]()
+                        { exec->spin(); });
+  std::jthread Actual_Battery_Thread([Batteryinstance]()
+                                     { Batteryinstance->Startup(); });
+
   spin_ros.join(); // Waits for the ROS to shutdown.
   Batteryinstance->Shutdown();
   rclcpp::shutdown();
-  
+
   return 0;
 }
